5ddac047640bbf7bef9eff516367f12acf369406
Ritu1698 <biswasrituparna83@gmail.com>
2020-10-06 00:12:09 +0530

    [Rituparna]Added: Welcome Message & Object Creation

---
+++ src/main/java/Hotel.java
@@ -0,0 +1,41 @@
+public class Hotel
+{
+    private String hotelName;
+    private int weekdayRate;
+    private int weekendRate;
+    private int rating;
+    public String getHotelName() {
+        return hotelName;
+    }
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+    public int getWeekdayRate() {
+        return weekdayRate;
+    }
+    public void setWeekdayRate(int weekdayRate) {
+        this.weekdayRate = weekdayRate;
+    }
+    public int getWeekendRate() {
+        return weekendRate;
+    }
+    public void setWeekendRate(int weekendRate) {
+        this.weekendRate = weekendRate;
+    }
+    public int getRating() {
+        return rating;
+    }
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+    public Hotel(String hotelName, int weekdayRate, int weekendRate, int rating) {
+        super();
+        this.hotelName = hotelName;
+        this.weekdayRate = weekdayRate;
+        this.weekendRate = weekendRate;
+        this.rating = rating;
+    }
+
+
+
+}
---
+++ src/main/java/HotelManagementSystem.java
@@ -0,0 +1,18 @@
+
+import java.text.ParseException;
+
+
+public class HotelManagementSystem {
+
+
+
+    public static void main(String args[]) throws ParseException {
+        System.out.println("Welcome to Hotel Reservation Program!");
+        Hotel lakewood = new Hotel("Lakewood", 110, 90, 3);
+        Hotel bridgewood = new Hotel("Bridgewood", 150, 50, 4);
+        Hotel ridgewood = new Hotel("Ridgewood", 220, 150, 5);
+
+    }
+
+
+}
